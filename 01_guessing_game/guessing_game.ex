15019bcf3fd280c762848674dfeead932cd9005f
= <=>
2021-01-02 22:00:25 -0500

    Day 1/100: finish lecture

---
+++ 01_guessing_game/guessing_game.ex
@@ -0,0 +1,42 @@
+defmodule GuessingGame do
+  # guess between a low number and a high number -> guess the middle
+  # tell user our guess
+  # "yes" -> game over
+  # "bigger" -> bigger(low, high)
+  # "smaller" -> smaller(low, high)
+  # anything else -> tell user to enter a valid response
+  def guess(a, b) when a > b, do: guess(b, a)
+
+  def guess(low, high) do
+    answer = IO.gets("Hmm... maybe you're thinking of #{mid(low, high)}?\n")
+
+    case String.trim(answer) do
+      "bigger" ->
+        bigger(low, high)
+
+      "smaller" ->
+        smaller(low, high)
+
+      "yes" ->
+        "I knew I could guess your number."
+
+      _ ->
+        IO.puts(~s{Type "bigger", "smaller", or "yes" (without the quotation marks)})
+        guess(low, high)
+    end
+  end
+
+  def mid(low, high) do
+    div(low + high, 2)
+  end
+
+  def bigger(low, high) do
+    new_low = min(high, mid(low, high) + 1)
+    guess(new_low, high)
+  end
+
+  def smaller(low, high) do
+    new_high = max(low, mid(low, high) - 1)
+    guess(low, new_high)
+  end
+end
